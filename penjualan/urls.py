14a5803825687470c9488ad8965536d1ecb55881
leopard1982 <99190737+leopard1982@users.noreply.github.com>
2023-09-02 16:16:41 +0700

    tambah voucher dll 02.09.2023

--- penjualan/urls.py
+++ penjualan/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from penjualan.views import dashboard, set_search_keyword, get_kategori, get_barang_cari5
-
+from penjualan.views import cari_keyword, hasil_cari
 urlpatterns = [
     path('',dashboard,name="dashboard"),
     path('api/set/search/',set_search_keyword, name="set_search_keyword"),
+    path('api/get/search/',cari_keyword, name="cari_keyword"),
+    path('api/get/search/hasil/',hasil_cari, name="hasil_cari"),
     path('api/get/kategori/',get_kategori,name="get_kategori"),
     path('api/get/barang/cari5/',get_barang_cari5,name="get_barang_cari5"),
 ]
--- penjualan/views.py
+++ penjualan/views.py
@@ -8,7 +8,7 @@ from rest_framework.decorators import api_view
 
 from rest_framework.response import Response
 
-from django.db.models import F
+from django.db.models import F, Q
 
 # Create your views here.
 def dashboard(request):
@@ -53,11 +53,46 @@ def get_kategori(request):
         serial = serialKategori(kategori,many=True)
         return Response(serial.data)
     return Response({})
+
+
 @api_view(['POST'])
 def get_barang_cari5(request):
     if request.method == 'POST':
         master = masterBarang.objects.all().order_by('-barang_dicari')[:5]
         serial = serialMasterBarang(master,many=True)
+        context = {
+            'result':serial.data
+        }
+        return Response(context)
+    return Response({})
+
+@api_view(['POST'])
+def cari_keyword(request):
+    if request.method == 'POST':
+        keyword_cari_stream=request.data['keyword'].strip().split(' ')
+        filternya = ""
+        q = Q()
+        for filter in keyword_cari_stream:
+            q = q | Q(barang_nama__icontains=filter)
+            print(q)
+        data = masterBarang.objects.filter(q)
+    
+        serial = serialMasterBarang(data,many=True)
+        context = {
+            'result':serial.data
+        }
+        return Response(context)
+    return Response({})
+
+@api_view(['POST'])
+def hasil_cari(request):
+    if request.method == 'POST':
+        filternya = request.data['kode_sku']
+        print(filternya)
+        data = masterBarang.objects.get(barang_sku=filternya)
+        data.barang_dicari = data.barang_dicari+1
+        data.save()
+        serial = serialMasterBarang(data)
         context = {
             'result':serial.data
         }
